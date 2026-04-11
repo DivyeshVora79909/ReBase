8f854a200dd63c4712c5f6d7b0da87272925b826
DIVYESH VORA <divy.r.vora14@gmail.com>
2026-04-11 14:04:24 +0530

    STABLE RELEASE: authorization engine and graph completed

--- test1.js
+++ test1.js
@@ -3,33 +3,26 @@ import { Surreal } from "surrealdb";
 const db = new Surreal();
 
 async function run() {
-  await db.connect("http://localhost:8000/rpc");
-
-  await db.signin({
-    namespace: "main",
-    database: "main",
-    access: "user",
-    variables: {
-      email: "admin@example.com",
-      password: "admin123",
-    },
-  });
-
-  const beforeRefresh = await db.query(`RETURN $auth;`);
-  console.log("before refresh", beforeRefresh);
-
-  const refresh = await db.query(`RETURN fn::refresh_access($auth);`);
-  console.log("refresh", refresh);
-
-  const afterRefresh = await db.query(`RETURN (SELECT * FROM $auth);`);
-  console.log("after refresh", afterRefresh);
-
   try {
-    const users = await db.query(`SELECT id, email FROM user;`);
-    console.log("\n✅ PERMISSION TEST SUCCESS! Fetched users:");
-    console.log(users[0]);
+    await db.connect("http://localhost:8000/rpc");
+
+    await db.signin({
+      namespace: "main",
+      database: "main",
+      access: "account",
+      variables: {
+        email: "admin@example.com",
+        password: "admin123",
+      },
+    });
+
+    console.log(await db.query("SELECT id, name FROM $auth;"));
+    console.log(await db.query(`SELECT * FROM user;`));
+    console.log(await db.query(`SELECT * FROM groups;`));
   } catch (error) {
-    console.log("\n❌ PERMISSION TEST FAILED:", error.message);
+    console.error("❌", error.message);
+  } finally {
+    db.close();
   }
 }
 
